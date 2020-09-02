1de5d35d8c18fe14e14e50a3f78dea0a41e420bb
cassidykang <ckang21@my.bcit.ca>
2020-09-02 15:52:36 -0700

    nav bar styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Logo from '../images/reeltalk-logo.png';
+
+
 
 const Header = () => (
 	<header>
-    	<h1><Link to={'/'}><img src={Logo}/></Link></h1>
+    	<h1><Link to={'/'}></Link></h1>
 	</header>
 );
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React, {useState} from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Spin as Hamburger } from 'hamburger-react';
+import Logo from '../images/reeltalk-logo.png';
 
-const Nav = () => (
-	<nav>
-        <ul>
+
+const Nav = () => {
+
+    const [open, setOpen] = useState(false);
+    const openHamburger = () => {
+        setOpen(!open);
+    }
+
+    return (
+    <nav>
+        <Hamburger onToggle={openHamburger} size={28}/>
+        <div className="logo"><Link to={'/'}><img src={Logo}/></Link></div>
+        {open && <ul >
             <li><NavLink to={'/'} exact>Home</NavLink></li>
             <li><NavLink to={'/about'}>About</NavLink></li>
             <li><NavLink to={'/favorites'}>Favorites</NavLink></li>
             <li><NavLink to={'/watch-later'}>Watch Later</NavLink></li>
-        </ul>
+        </ul>}
 	</nav>
-);
+    );
+};
 
 export default Nav;
