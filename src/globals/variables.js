e8902737fe923bd55d0bf6bfa6838f657f6bc146
aks <aaronksham@gmail.com>
2020-09-02 16:54:39 -0700

    Revert "Front page filters"
    This reverts commit 769d73b583cce6c7290ba115842a2a721a36f8c8.

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -9,8 +9,8 @@ export const BASE_URL = "https://api.themoviedb.org/3/movie/";
 
 //FILTERS
 // BASE_URL + URL_FILTER.xyz + API_KEY
-export const URL_FILTER = {pop: "popular", now: "now_playing", top: "top_rated", soon: "upcoming"};
+export const URL_FILTER = {now: "now_playing", pop: "popular", top: "top_rated", soon: "upcoming"};
 
 // Default Choices
-export const DEFAULT_FILTER = 'pop';
+export const DEFAULT_CITY = 'Popular';
 export const DEFAULT_YEAR = '2020';
