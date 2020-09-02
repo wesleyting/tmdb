649f8baf503c6d83316502f6efa99cf9a990871f
cassidykang <ckang21@my.bcit.ca>
2020-09-02 12:08:15 -0700

    logo added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Logo from '../images/reeltalk-logo.png';
 
 const Header = () => (
 	<header>
-    	<h1><Link to={'/'}>ReelTalk Logo</Link></h1>
+    	<h1><Link to={'/'}><img src={Logo}/></Link></h1>
 	</header>
 );
 
