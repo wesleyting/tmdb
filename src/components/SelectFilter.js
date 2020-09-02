769d73b583cce6c7290ba115842a2a721a36f8c8
aks <aaronksham@gmail.com>
2020-09-02 16:50:47 -0700

    Front page filters
    Added SelectList component - dropdown that changes the views

---
+++ src/components/SelectFilter.js
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+
+const SelectFilter = ({handleChange, defaultFilter, filterChoices}) => {
+
+    const handleFilter = (e) => {
+        handleChange(e.target.value);
+    }
+
+    return <select onChange={handleFilter} defaultValue={defaultFilter}>
+            {Object.entries(filterChoices).map((item, i) => {
+                // format the string
+                let itemKey = item[0];
+                let itemValue, itemValueFormat = item[1].split("_");
+                for (let j = 0; j < itemValueFormat.length; j++) {
+                    itemValueFormat[j] = itemValueFormat[j][0].toUpperCase()+itemValueFormat[j].slice(1);
+                    itemValue = (j > 0) ? itemValueFormat[j-1]+" "+itemValueFormat[j] : itemValueFormat[j];
+                }
+
+                return(
+                    <option key={i} value={itemKey}>{itemValue}</option>
+                );
+    })};
+    </select>
+    
+
+}
+
+export default SelectFilter;
