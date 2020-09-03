3e9d1aac55f98f515ad0cc5300009963a6f3a126
cassidykang <ckang21@my.bcit.ca>
2020-09-02 21:25:12 -0700

    footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,8 +5,10 @@ const y = d.getFullYear();
 
 const Footer = () => (
     <footer>
-        <p>&copy; {y} | Reel Talk</p>
-        <p>Created by <a href="http://asham.bcitwebdeveloper.ca/">Aaron Sham</a>, <a href="http://ckang.bcitwebdeveloper.ca/">Cassidy Kang</a>, <a href="http://wting.bcitwebdeveloper.ca/">Wesley Ting</a></p>
+        <div className="footer-content">
+            <p>&copy; {y} | Reel Talk</p>
+            <p>Created by <a href="http://asham.bcitwebdeveloper.ca/">Aaron Sham</a>, <a href="http://ckang.bcitwebdeveloper.ca/">Cassidy Kang</a>, <a href="http://wting.bcitwebdeveloper.ca/">Wesley Ting</a></p>
+        </div>
     </footer>
 );
 
