89024e138a2a5e956bfd03c64779261cece2d957
aks <aaronksham@gmail.com>
2020-09-02 17:02:08 -0700

    Added filter dropdown
    SelectList component as well as updating the API call to pull new movies

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,25 +1,30 @@
 import React, { useEffect, useState } from 'react';
-import { BASE_URL, API_KEY, URL_FILTER } from '../globals/variables.js'
+import { BASE_URL, API_KEY, URL_FILTER, DEFAULT_FILTER } from '../globals/variables.js'
 import MovieList from './MovieList';
+import SelectFilter from './SelectFilter';
 
 const Home = () => {
 
     const [grid, setGrid] = useState(null);
+    const [filter, setFilter] = useState(DEFAULT_FILTER);
+
+    const handleFilter = (e) => {
+        setFilter(e);
+    }
 
     
     useEffect(() => {
         const fetchMovie = async () => {
-            const res = await fetch(BASE_URL + URL_FILTER.now + API_KEY);
+            const res = await fetch(BASE_URL + URL_FILTER.[filter] + API_KEY);
             const data = await res.json();
             setGrid(data.results);
         }
         fetchMovie();
-    }, []); //changes to vars/obj within [] determines rate of change/update
+    }, [filter]); //changes to vars/obj within [] determines rate of change/update
     
-    
-
     return (
         <main>
+            <SelectFilter handleChange={handleFilter} filterChoices={URL_FILTER} defaultFilter={DEFAULT_FILTER} />
             <MovieList grid={grid}/>
         </main>
     );
