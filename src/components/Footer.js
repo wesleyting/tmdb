5688663495ff7d9d0e2e7af07d2d966ee94a4d99
cassidykang <ckang21@my.bcit.ca>
2020-09-03 21:00:23 -0700

    footer adjustments

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,9 @@ const d = new Date();
 const y = d.getFullYear();
 
 const Footer = () => (
-    <footer>
-        <div className="footer-content">
+    <footer className="footer-content">
             <p>&copy; {y} | Reel Talk</p>
             <p>Created by <a href="http://asham.bcitwebdeveloper.ca/">Aaron Sham</a>, <a href="http://ckang.bcitwebdeveloper.ca/">Cassidy Kang</a>, <a href="http://wting.bcitwebdeveloper.ca/">Wesley Ting</a></p>
-        </div>
     </footer>
 );
 
