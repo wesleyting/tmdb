46ecef14ca8296995b2696586f4a9167bccf9dbe
cassidykang <ckang21@my.bcit.ca>
2020-09-16 20:02:51 -0700

    page not found js file updated

--- src/components/PageNotFound.js
+++ src/components/PageNotFound.js
@@ -3,12 +3,13 @@ import { Link } from 'react-router-dom';
 
 const PageNotFound = () => (
 	<main>
-		<section>
-			<div className="page-info">
-				<h2>404...Page Not Found</h2>
+		<section className = "page-not-found">
+			<div className="not-found-info">
+				<h1>404 <br></br>Page Not Found</h1>
+				<p>Oops! The page you are trying to view does not exist or an error occurred.</p>
 			</div>
-			<div className="about-text">
-				<p>Go to the <Link to={'/'}>home</Link> page.</p>
+			<div className="not-found-text">
+				<p>Click <Link to={'/'}>here</Link> to go back to the home page.</p>
 			</div>
 		</section>
 	</main>
