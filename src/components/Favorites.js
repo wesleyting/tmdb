787fe48141a2eba8b4cffd32379caf79627d3b17
cassidykang <ckang21@my.bcit.ca>
2020-09-17 19:39:02 -0700

    favourite page styles added

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import MovieList from './MovieList';
 import SearchBar from './SearchBar';
 import { getFaves, clearFaves } from '../utilities/faveStorage';
+import { Link } from 'react-router-dom';
 
 const Favorites = () => {
     let [grid, setGrid] = useState(getFaves('faves'));
@@ -15,17 +16,24 @@ const Favorites = () => {
         if (grid === null || (Array.isArray(grid) && grid.length === 0)) {
             return(
                 <main>
-                    <div className="content-wrapper">
-                        <p>You have no favorites added.</p>
-                        <SearchBar type="fave" />    
+                    <h1 className='fave-title'>Your Favourite Movies</h1>
+                    <div className="fave-wrapper">
+                        <p>You currently have no favorites added!</p>
+                        <p>Go back to the <Link to={'/'}>home page</Link> to browse through movies, or search for a specific movie.</p>
+                        <div className='search-btn-data'>
+                            <SearchBar type="fave"/>
+                        </div>    
                     </div>
                 </main>
             );
         } else {
             return (
                 <main>
+                    <h1 className='fave-title'>Your Favourite Movies</h1>
                     <div className="content-wrapper">
-                        <SearchBar type="fave" />    
+                        <div className='search-btn-data'>
+                            <SearchBar type="fave"/>
+                        </div>    
                         <button id="cl-faves" onClick={handleClear}>Clear All</button>
                         <div className="movie-grid"><MovieList grid={grid}/></div>
                     </div>
