e76dfa0350d5966e5c06174956e1de518ab8ac47
aks <aaronksham@gmail.com>
2020-09-18 12:11:31 -0700

    Blank search to search page

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -26,16 +26,42 @@ const SearchResults = ({match}) => {
         fetchMovie();
     }, [page, query]);
 
+    const doResult = () => {
+        if (!grid || (Array.isArray(grid) && grid.length === 0)) {
+            return (
+                <div className="content-wrapper">
+                    <div className="search-crumb">
+                        <h2>No results for "{query}".</h2>
+                        <p>Please try another search.</p>
+                        <SearchBar />
+                    </div>
+                </div>
+            );
+        } else if (Array.isArray(grid) && grid.length > 0) {
+            return (
+            <div className="content-wrapper">
+                <SearchBar q={query}/>
+                <div className="search-crumb">
+                    <h2>Showing results for "{query}":</h2>
+                </div>
+                <div className="movie-grid"><MovieList grid={grid}/></div>
+                <Pagination results={pagi} handleChange={handlePagi}/>
+            </div>
+            ); 
+        }
+    }
+
     return (
         <main>
-        <div className="content-wrapper">
-            <SearchBar q={query}/>
-            <div className="search-crumb">
-                <h2>Showing results for "{query}":</h2>
-            </div>
-            <div className="movie-grid"><MovieList grid={grid}/></div>
-            <Pagination results={pagi} handleChange={handlePagi}/>
-        </div>
+            {/* <div className="content-wrapper">
+                <SearchBar q={query}/>
+                <div className="search-crumb">
+                    <h2>Showing results for "{query}":</h2>
+                </div>
+                <div className="movie-grid"><MovieList grid={grid}/></div>
+                <Pagination results={pagi} handleChange={handlePagi}/>
+            </div> */}
+            {doResult()}
         
         </main>
     )
