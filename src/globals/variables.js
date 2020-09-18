10565a64ae4039368ae25c01e3f4e3be9fe09314
aks <aaronksham@gmail.com>
2020-09-17 22:27:20 -0700

    Created no-poster.png
    Updated variables.js

--- src/globals/variables.js
+++ src/globals/variables.js
@@ -1,25 +1,22 @@
-import PLACEHOLDER_POSTER from '../images/reeltalk-logo.png';
+import PLACEHOLDER_POSTER from '../images/no-poster.png';
 
 // DEFAULTS
 export const DEFAULT_FILTER = "pop";
-export const DEFAULT_YEAR = "2020";
 export const DEFAULT_PAGE = 1;
 
 //API KEY
 const API_KEY_ONLY = "a109986a8c819f27263237cea5bc599a";
 export const API_KEY = `?api_key=${API_KEY_ONLY}`;
-// base
+
 // base_url + {movie id} + api_key + base_lang
 export const BASE_URL = "https://api.themoviedb.org/3/movie/";
 export const BASE_SEARCH = "https://api.themoviedb.org/3/search/movie/";
-// export const BASE_LANG = `&language="en-US"`;
-export const BASE_PAGE = `&page=`;
+export const BASE_LANG = '&language="en-US"';
+export const BASE_PAGE = "&page=";
 
 //FILTERS
 // BASE_URL + URL_FILTER.xyz + API_KEY
 export const URL_FILTER = {pop: "popular", now: "now_playing", top: "top_rated", soon: "upcoming"};
-
-
 
 // Movie Box Constants
 export const MONTH_FORMAT = "long"; //use short for abbrv
