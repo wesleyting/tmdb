4c6998076da822893e26c928ac90e9b56de711c3
aks <aaronksham@gmail.com>
2020-09-18 18:32:32 -0700

    Pagination numbers
    Should be responsive as well: note that it does break in chrome devtools mobile view because reasons (seems to work fine in Firefox inspector and regular chrome window view)

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import getWinDim from '../utilities/getWinDim';
 
 const Pagination = ( {results, handleChange} ) => {
 
@@ -6,22 +7,74 @@ const Pagination = ( {results, handleChange} ) => {
     let curr = results !== null ? results.page : 1;
     let minDis = curr <= 1 ? true : false;
     let maxDis = curr === total ? true: false;
-    // console.log(`totalPages:${total}`, `currPages:${curr}`);
-
     const pageNavs = [];
+
+    const calcPagiPer = (dims) => {
+        if (dims >= 760) {
+            return 10;
+        } else if (dims >= 500) {
+            return 5;
+        } else {
+            return 0;
+        }
+    }
+
+    const handleResize = () => {
+        return setWinWidth(getWinDim("width"));        
+    }
+
+    const [winWidth, setWinWidth] = useState(getWinDim("width"));
+    const [maxPagiPerPage, setPagiPer] = useState(calcPagiPer(winWidth));
+    window.removeEventListener('resize', handleResize, false);
+    window.addEventListener('resize', handleResize, false);
+
+    useEffect( () => {
+        let mounted = true;
+        setWinWidth(getWinDim("width"));
+        if (mounted === true) {
+            setPagiPer(calcPagiPer(getWinDim("width")));
+        } 
+        return () => mounted = false;
+    }, [winWidth]);    
 
     const handlePagi = (p) => {
         handleChange(p);
-    }   
+    }  
+    
+    const currPageClass = (idx) => {
+        if (curr === idx) {
+            return "selected";
+        } else {
+            return "";
+        }
+    }
 
-    if (total > 1) {
-        pageNavs.push(<div key="0" ><button className="pagi-prev" onClick={() => {handlePagi(curr-1)}} disabled={minDis}>Previous</button></div>);
-        pageNavs.push(<div key="1" ><button className="pagi-next" onClick={() => {handlePagi(curr+1)}} disabled={maxDis}>Next</button></div>);
+    const doPagiNums = () => {
+        if (curr > Math.floor(maxPagiPerPage / 2)) {
+            for (let i = curr - Math.floor(maxPagiPerPage / 2); i < curr + Math.ceil(maxPagiPerPage / 2); i++) {
+                pageNavs.push(<div key={i}><button className={`pagi-pg ${currPageClass(i)}`} onClick={() => {handlePagi(i)}}>{i}</button></div>);
+            }
+        } else {
+            for (let i = 1; i <= maxPagiPerPage; i++) {
+                pageNavs.push(<div key={i}><button className={`pagi-pg ${currPageClass(i)}`} onClick={() => {handlePagi(i)}}>{i}</button></div>);
+            }
+        }
+    }
+
+    const doPagi = () => {
+        if (total > 1) {
+            pageNavs.push(<div key="previous" ><button className="pagi-prev" onClick={() => {handlePagi(curr-1)}} disabled={minDis}>Previous</button></div>);
+            if (maxPagiPerPage > 0) {
+                doPagiNums();
+            }
+            pageNavs.push(<div key="next" ><button className="pagi-next" onClick={() => {handlePagi(curr+1)}} disabled={maxDis}>Next</button></div>);
+        }
+        return pageNavs;
     }
 
     return (
         <div className="bottom-pagination">
-            <div className="pagi-wrap">{pageNavs}</div>
+            <div className="pagi-wrap">{doPagi()}</div>
         </div>
 
     )
